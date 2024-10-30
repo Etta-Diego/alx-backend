5873d139077c4e78b8cb922b80a07cc2232ada2b
Etta-Diego <{ID}+{Etta-Diego}@users.noreply.github.com>
2024-10-30 11:05:12 +0100

    fix pycodestyle error in 2-hypermedia_pagination.py* file

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -89,7 +89,6 @@ class Server:
         assert isinstance(page, int) and page > 0
 
         data = self.get_page(page, page_size)
-        
         total_pages = math.ceil(len(self.dataset()) / page_size)
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
